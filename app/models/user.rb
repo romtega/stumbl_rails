d12798d60e5ec9adab72f3bc3976daea8afb4fac
Roman <romtega88@gmail.com>
2016-03-19 16:56:45 -0600

    Installed Devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+
+	validates_presence_of :username
+	validates_uniqueness_of :username
+	
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
